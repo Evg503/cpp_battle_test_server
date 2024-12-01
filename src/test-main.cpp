95ee1afd0f83e58bbc75d17064bf325e2e71a4c6
Evgeny <evgeny.kochenyuk@gmail.com>
2024-12-01 14:25:26 +0100

    class TestSuite

--- src/test-main.cpp
+++ src/test-main.cpp
@@ -1,6 +1,6 @@
+#include "Game.hpp"
 #include "Tests/AssertLog.hpp"
 #include "Units/types.hpp"
-#include "Game.hpp"
 
 #include <cassert>
 #include <deque>
@@ -8,71 +8,79 @@
 #include <iostream>
 
 template <template <typename> typename Game, typename TestLogger>
-void testMapSize()
+class TestSuite
 {
+private:
 	TestLogger log;
-	Game<TestLogger> game(log);
+	Game<TestLogger> game;
+	static int testcounter;
 
-	game.createField(4, 9);
-	log.expect("[1] MAP_CREATED width=4 height=9 ");
-}
+public:
+	TestSuite() :
+			log(),
+			game(log)
 
-template <template <typename> typename Game, typename TestLogger>
-void testCreateGame()
-{
-	TestLogger log;
-	Game<TestLogger> game(log);
+	{}
+    static int getCounter()
+    {
+        return testcounter;
+    }
+	void testMapSize()
+	{
+		game.createField(4, 9);
+		log.expect("[1] MAP_CREATED width=4 height=9 ");
+        ++testcounter;
+	}
 
-	game.createField(10, 10);
-	log.expect("[1] MAP_CREATED width=10 height=10 ");
+	void testCreateGame()
+	{
+		game.createField(10, 10);
+		log.expect("[1] MAP_CREATED width=10 height=10 ");
 
-	game.spawn("SWORDMAN", 1, 0, 0, 5, 2);
-	log.expect("[1] UNIT_SPAWNED unitId=1 unitType=Swordsman x=0 y=0 ");
+		game.spawn("SWORDMAN", 1, 0, 0, 5, 2);
+		log.expect("[1] UNIT_SPAWNED unitId=1 unitType=Swordsman x=0 y=0 ");
 
-	game.spawn("HUNTER", 2, 9, 0, 10, 5, 1, 4);
-	log.expect("[1] UNIT_SPAWNED unitId=2 unitType=Hunter x=9 y=0 ");
+		game.spawn("HUNTER", 2, 9, 0, 10, 5, 1, 4);
+		log.expect("[1] UNIT_SPAWNED unitId=2 unitType=Hunter x=9 y=0 ");
 
-	game.march(1, 9, 0);
-	log.expect("[1] MARCH_STARTED unitId=1 x=0 y=0 targetX=9 targetY=0 ");
-	game.march(2, 0, 0);
-	log.expect("[1] MARCH_STARTED unitId=2 x=9 y=0 targetX=0 targetY=0 ");
-	game.spawn("SWORDMAN", 3, 0, 9, 10, 2);
-	log.expect("[1] UNIT_SPAWNED unitId=3 unitType=Swordsman x=0 y=9 ");
-	game.march(3, 0, 0);
-	log.expect("[1] MARCH_STARTED unitId=3 x=0 y=9 targetX=0 targetY=0 ");
-}
+		game.march(1, 9, 0);
+		log.expect("[1] MARCH_STARTED unitId=1 x=0 y=0 targetX=9 targetY=0 ");
+		game.march(2, 0, 0);
+		log.expect("[1] MARCH_STARTED unitId=2 x=9 y=0 targetX=0 targetY=0 ");
+		game.spawn("SWORDMAN", 3, 0, 9, 10, 2);
+		log.expect("[1] UNIT_SPAWNED unitId=3 unitType=Swordsman x=0 y=9 ");
+		game.march(3, 0, 0);
+		log.expect("[1] MARCH_STARTED unitId=3 x=0 y=9 targetX=0 targetY=0 ");
+        ++testcounter;
+	}
 
-template <template <typename> typename Game, typename TestLogger>
-void testGameLoop()
-{
-	TestLogger log;
-	Game<TestLogger> game(log);
+	void testGameLoop()
+	{
+		game.createField(10, 10);
+		game.spawn("SWORDMAN", 1, 0, 0, 5, 2);
+		game.spawn("HUNTER", 2, 9, 0, 10, 5, 1, 4);
+		game.march(1, 9, 0);
+		game.march(2, 0, 0);
+		game.spawn("SWORDMAN", 3, 0, 9, 10, 2);
+		game.march(3, 0, 0);
 
-	game.createField(10, 10);
-	game.spawn("SWORDMAN", 1, 0, 0, 5, 2);
-	game.spawn("HUNTER", 2, 9, 0, 10, 5, 1, 4);
-	game.march(1, 9, 0);
-	game.march(2, 0, 0);
-	game.spawn("SWORDMAN", 3, 0, 9, 10, 2);
-	game.march(3, 0, 0);
+		log.forget();
+		game.update();
+		log.expect("[2] UNIT_MOVED unitId=1 x=1 y=0 ");
+		log.expect("[2] UNIT_MOVED unitId=2 x=8 y=0 ");
+		log.expect("[2] UNIT_MOVED unitId=3 x=0 y=8 ");
 
-	log.forget();
-	game.update();
-	log.expect("[2] UNIT_MOVED unitId=1 x=1 y=0 ");
-	log.expect("[2] UNIT_MOVED unitId=2 x=8 y=0 ");
-	log.expect("[2] UNIT_MOVED unitId=3 x=0 y=8 ");
+		game.update();
+		log.expect("[3] UNIT_MOVED unitId=1 x=2 y=0 ");
+		log.expect("[3] UNIT_MOVED unitId=2 x=7 y=0 ");
+		log.expect("[3] UNIT_MOVED unitId=3 x=0 y=7 ");
 
-	game.update();
-	log.expect("[3] UNIT_MOVED unitId=1 x=2 y=0 ");
-	log.expect("[3] UNIT_MOVED unitId=2 x=7 y=0 ");
-	log.expect("[3] UNIT_MOVED unitId=3 x=0 y=7 ");
-
-	// game.update();
-	// log.expect("[4] UNIT_MOVED unitId=1 x=3 y=0 ");
-	// log.expect("[4] UNIT_ATTACKED attackerUnitId=2 targetUnitId=1 damage=5 targetHp=0 ");
-	// log.expect("[4] UNIT_DIED unitId=1 ");
-	// log.expect("[4] UNIT_MOVED unitId=3 x=0 y=6 ");
-	/*
+		// game.update();
+		// log.expect("[4] UNIT_MOVED unitId=1 x=3 y=0 ");
+		// log.expect("[4] UNIT_ATTACKED attackerUnitId=2 targetUnitId=1 damage=5 targetHp=0 ");
+		// log.expect("[4] UNIT_DIED unitId=1 ");
+		// log.expect("[4] UNIT_MOVED unitId=3 x=0 y=6 ");
+		/*
 [5] UNIT_MOVED unitId=2 x=6 y=0 
 [5] UNIT_MOVED unitId=3 x=0 y=5 
 [6] UNIT_MOVED unitId=2 x=5 y=0 
@@ -83,13 +91,19 @@ void testGameLoop()
 [8] UNIT_DIED unitId=3 
 
      */
-}
-
+        ++testcounter;
+	}
+};
+// template <>
+// int TestSuite<Game, AssertEventLog>::testcounter = 0;
+template <template <typename> typename Game, typename TestLogger>
+int TestSuite<Game, TestLogger>::testcounter = 0;
+;
 int main(int argc, char** argv)
 {
-	testMapSize<Game, AssertEventLog>();
-	testCreateGame<Game, AssertEventLog>();
-	testGameLoop<Game, AssertEventLog>();
+	TestSuite<Game, AssertEventLog>().testMapSize();
+	TestSuite<Game, AssertEventLog>().testCreateGame();
+	TestSuite<Game, AssertEventLog>().testGameLoop();
 
-	std::cout << "TESTS PASSED!!!\n";
+	std::cout << TestSuite<Game, AssertEventLog>::getCounter() << " TESTS PASSED!!!\n";
 }
